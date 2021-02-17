862daa3199aa0621d844d71596f937f7582c883c
Jeffrey Jang <jeff.jang86@gmail.com>
2021-02-16 21:40:48 -0600

    updating

--- Assets/LootKingdom/CommonScripts/Backend/ClientAPI.cs
+++ Assets/LootKingdom/CommonScripts/Backend/ClientAPI.cs
@@ -4,71 +4,74 @@ using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Threading.Tasks;
 
 public class ClientAPI : MonoBehaviour {
-    private string url = "localhost:3000/users";
+    private string usersURL = "localhost:3000/users";
     private static System.Random random = new System.Random();
 
     void Start() {
-        User user = new User() {
-            user_id = "87654321",
-            email = "bennida.c@gmail.com",
-            first_name = "bennida",
-            last_name = "jang",
-            user_name = "bjang"
-        };
-        
-        StartCoroutine(GetAllUsers());
-        // StartCoroutine(GetUserById("12345678"));
-
-        // StartCoroutine(CreateUser(user));
-        // StartCoroutine(GetAllUsers());
-
-        // StartCoroutine(DeleteUserById("JR239PS0"));
-        // StartCoroutine(GetAllUsers());
+    //     User user = new User() {
+    //         user_id = "87654321",
+    //         email = "bennida.c@gmail.com",
+    //         first_name = "bennida",
+    //         last_name = "jang",
+    //         user_name = "bjang"
+    //     };
+    //     await GetAllUsers();
+    //     Debug.Log("First");
+    //     await GetUserById("12345678");
+    //     Debug.Log("Second");
+    //     await CreateUser(user);
+    //     Debug.Log("Third");
+    //     await GetAllUsers();
+    //     Debug.Log("Fourth");
+    //     await DeleteUserById("ZGHLLJIM");
+    //     await GetAllUsers();
     }
 
-    public IEnumerator GetAllUsers() {
-        using(UnityWebRequest www = UnityWebRequest.Get($"{url}")){
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError) {
-                Debug.Log(www.error);
+    public async Task<List<User>> GetAllUsers() {
+        using(UnityWebRequest www = UnityWebRequest.Get($"{usersURL}")){
+            www.SendWebRequest(); 
+            while (!www.isDone) {
+                if (www.isNetworkError) {
+                    Debug.Log(www.error);
+                    break;
+                }
+                await Task.Delay(10);
+            }
+            if (www.isDone) {
+                var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(result);
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(result);
+                return users;
             }
             else {
-                if (www.isDone) {
-                    var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
-                    Debug.Log(result);
-                }
-                else {
-                    Debug.Log("Error! Couldn't get data.");
-                }
+                Debug.Log("Error! Couldn't get data.");
+                return new List<User>();
             }
         }
     }
 
-    public IEnumerator GetUserById(string id) {
-        using(UnityWebRequest www = UnityWebRequest.Get($"{url}/{id}")){
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError) {
-                Debug.Log(www.error);
+    public async Task<User> GetUserById(string id) {
+        using(UnityWebRequest www = UnityWebRequest.Get($"{usersURL}/{id}")){
+            www.SendWebRequest();
+            while (!www.isDone) {
+                if (www.isNetworkError) {
+                    Debug.Log(www.error);
+                    break;
+                }
+                await Task.Delay(1);
+            }
+            if (www.isDone) {
+                var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                Debug.Log(result);
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(result);
+                return users[0];
             }
             else {
-                if (www.isDone) {
-                    var result = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
-                    Debug.Log(result);
-                    List<User> users = JsonConvert.DeserializeObject<List<User>>(result);
-
-                    Debug.Log(users[0].user_id);
-                    Debug.Log(users[0].email);
-                    Debug.Log(users[0].user_name);
-                    Debug.Log(users[0].first_name);
-                    Debug.Log(users[0].last_name);
-                }
-                else {
-                    Debug.Log("Error! Couldn't get data.");
-                }
+                Debug.Log("Error! Couldn't get data.");
+                return new User();
             }
         }
     }
@@ -93,7 +96,7 @@ public class ClientAPI : MonoBehaviour {
     // }
 
     // Use "POST" to upload new data
-    IEnumerator CreateUser(User user) {
+    public async Task CreateUser(User user) {
         WWWForm form = new WWWForm();
         form.AddField("user_id", RandomString(8));
         form.AddField("email", user.email);
@@ -101,33 +104,37 @@ public class ClientAPI : MonoBehaviour {
         form.AddField("last_name", user.last_name);
         form.AddField("user_name", user.user_name);
 
-        using (UnityWebRequest www = UnityWebRequest.Post(url, form)) {
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError) {
-                Debug.Log(www.error);
+        using (UnityWebRequest www = UnityWebRequest.Post(usersURL, form)) {
+            www.SendWebRequest();
+            while (!www.isDone) {
+                if (www.isNetworkError) {
+                    Debug.Log(www.error);
+                    return;
+                }
+                await Task.Delay(1);
             }
-            else {
-                Debug.Log("Form upload complete!");
-            }
+            Debug.Log("Form upload complete!");
         }
     }
 
     // Use "PUT" to update data
-    public IEnumerator DeleteUserById(string id){
-        using(UnityWebRequest www = UnityWebRequest.Delete($"{url}/{id}")){
-            yield return www.SendWebRequest();
+    public async Task DeleteUserById(string id){
+        using(UnityWebRequest www = UnityWebRequest.Delete($"{usersURL}/{id}")){
+            www.SendWebRequest();
 
-            if (www.isNetworkError) {
-                Debug.Log(www.error);
+            while (!www.isDone) {
+                if (www.isNetworkError) {
+                    Debug.Log(www.error);
+                    break;
+                }
+                await Task.Delay(1);
+            }
+            if (www.isDone) {
+                Debug.Log($"User with ID: {id} has been deleted.");
             }
             else {
-                if (www.isDone) {
-                    Debug.Log($"User with ID: {id} has been deleted.");
-                }
-                else {
-                    Debug.Log("Error! Couldn't get data.");
-                }
+                Debug.Log("Error! Couldn't get data.");
+            
             }
         }
     }
